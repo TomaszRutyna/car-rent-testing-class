aff69f54f023ade28b888436b7f679d688bde263
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2019-08-25 13:33:31 +0200

    testy MVC

--- src/test/java/pl/sda/carrent/integration/MainViewMVCIT.java
+++ src/test/java/pl/sda/carrent/integration/MainViewMVCIT.java
@@ -3,19 +3,19 @@ package pl.sda.carrent.integration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pl.sda.carrent.infrastructure.MainViewController;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(MainViewController.class)
 public class MainViewMVCIT {
 
     @Autowired
